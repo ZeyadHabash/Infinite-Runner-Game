a92272b1ad735635e8486cd38617bba3a61e6fc4
Zeyad Habash <zeyad.habashy02@gmail.com>
2024-10-26 09:32:19 +0300

    Enhance tile spawning logic to ensure normal tiles are included for gameplay balance

--- Assets/Game/Scripts/TileSpawner.cs
+++ Assets/Game/Scripts/TileSpawner.cs
@@ -70,31 +70,41 @@ namespace InfiniteRunner
         #region Private Methods
         private void SpawnTileGroup()
         {
+            int tileToBeNormal = Random.Range(0, 3);
+
+            // A tile is chosen to be normal if it is the first spawn
+            // or randomly after the first spawn (1/3 chance)
+            // Just to make sure that there are normal tiles every row so the game is playable
+            // (game is too easy now :( )
+
             // Spawn Left Tile
-            SpawnTile(_spawnPosition + Vector3.left * 5);
+            SpawnTile(_spawnPosition + Vector3.left * 5, tileToBeNormal == 0 || _firstSpawn);
             // Spawn Middle Tile
-            SpawnTile(_spawnPosition);
+            SpawnTile(_spawnPosition, tileToBeNormal == 1 || _firstSpawn);
             // Spawn Right Tile
-            SpawnTile(_spawnPosition + Vector3.right * 5);
+            SpawnTile(_spawnPosition + Vector3.right * 5, tileToBeNormal == 2 || _firstSpawn);
+
+            // Update spawn position
             _spawnPosition += Vector3.forward * 5;
         }
 
-        private void SpawnTile(Vector3 position)
+        private void SpawnTile(Vector3 position, bool isNormal = false)
         {
-            PoolableMonoBehaviour tileGameObject = _tilePool.Get();
-            if (tileGameObject.TryGetComponent(out Tile tile))
+            PoolableMonoBehaviour tileGameObject = _tilePool.Get(); // Get a tile from the pool
+            if (tileGameObject.TryGetComponent(out Tile tile)) // Get the Tile component from the tileGameObject
             {
-                int configIndex = _firstSpawn ? 0 : Random.Range(0, _tileConfigs.Length);
-                tile.Configure(_tileConfigs[configIndex], position);
-                if (position.x == 0)
-                    tile.onRelease += OnMiddleTileReleased;
+                // if isNormal, only spawn normal tiles. else, spawn random tiles
+                int configIndex = isNormal ? 0 : Random.Range(0, _tileConfigs.Length);
+                tile.Configure(_tileConfigs[configIndex], position); // Configure the tile
+                if (position.x == 0) // If the tile is in the middle
+                    tile.onRelease += OnMiddleTileReleased; // Subscribe to the onRelease event
             }
         }
 
         private void OnMiddleTileReleased(PoolableMonoBehaviour tile)
         {
-            tile.onRelease -= OnMiddleTileReleased;
-            SpawnTileGroup();
+            tile.onRelease -= OnMiddleTileReleased; // Unsubscribe from the onRelease event
+            SpawnTileGroup(); // Spawn a new tile group after this tilegroup is released
         }
         #endregion
     }
